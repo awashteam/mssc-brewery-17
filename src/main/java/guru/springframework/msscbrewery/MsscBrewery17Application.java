6173a4c36a311378975f70d3f6abf0c53e6f30d7
awash <Emerald190!!>
2024-08-29 17:48:06 -0500

    add mssc-brewery-17

---
+++ src/main/java/guru/springframework/msscbrewery/MsscBrewery17Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewery17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBrewery17Application.class, args);
+	}
+
+}
---
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -0,0 +1,12 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+/**
+ * Created by tg 8/29/2024.
+ */
+public interface BeerService {
+    BeerDto getBeerById(UUID beerId);
+}
